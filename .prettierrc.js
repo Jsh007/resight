f5b7dd3344af6963141abb9d477927e269958754
Joshua Eigbe <jeigbe@gmail.com>
2024-01-29 23:35:56 +0100

    fix: updated manifest and pod files

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,27 @@
+/*
+ * @Author: Joshua Eigbe self@joshuaeigbe.com
+ * @Github: https://github.com/jsh007
+ * @Date: 2023-12-09 15:18:33
+ * @LastEditors: Joshua Eigbe self@joshuaeigbe.com
+ * @LastEditTime: 2023-12-12 21:30:50
+ * @FilePath: /resight/.prettierrc.js
+ * @copyrightText: Copyright (c) Joshua Eigbe. All Rights Reserved.
+ * @Description: See Github repo
+ */
 module.exports = {
-  arrowParens: 'avoid',
   bracketSameLine: true,
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+
+  arrowParens: 'always',
+  bracketSpacing: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  quoteProps: 'as-needed',
+  singleQuote: true,
+  semi: true,
+  printWidth: 100,
+  useTabs: true,
+  trailingComma: 'es5',
 };
